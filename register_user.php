ece1532cc305a1c18470008e8bbe4cfc0a0550da
aliabdullvh <aleeabdullah58@gmail.com>
2024-12-23 00:50:29 +0500

    Making the Login form

--- register_user.php
+++ register_user.php
@@ -1,12 +1,29 @@
 <?php
-
 include("header.php");
-
 ?> 
-
+<div class="container">
+<div class="card myCard">
+  <div class="card-header">
+    Register User
+  </div>
+  <div class="card-body">
+    <div class="row">
+        <div class="col-12">
+        <div class="mb-3">
+            <label for="" class="form-label">User Name</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">User Password</label>
+            <input type="password" class="form-control">
+        </div>
+    </div>
+    </div>
+    
+  </div>
+</div>
+</div>
 
 <?php
-
 include("footer.php");
-
 ?> 
